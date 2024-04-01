ad61dcc68e7ac9f77016af4aa71c482416aab4ea
Huma Meraj <162187792+HumaMeraj@users.noreply.github.com>
2024-04-02 01:03:48 +0500

    File of q52

---
+++ q52.js
@@ -0,0 +1,11 @@
+//Make a Smartphone Object: Create a simple way to keep track of a smartphone's details. Include its brand, model, and other key features like how much storage it has, the size of its screen, and how long its battery lasts.
+var smartphone = {
+    make: "Samsung",
+    model: "Galaxy S21",
+    specs: {
+        storage: "128GB",
+        screenSize: "6.2 inches",
+        batterylife: "20 hours"
+    }
+};
+console.log(smartphone);
